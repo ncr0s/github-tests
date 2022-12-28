8285ce4cfdf909752180cd83f2d91853493f65f7
Aleksei Volosovich <n14cr0s@gmail.com>
2022-12-28 11:26:20 +0200

    src/test/java/github/BestContributorToSelenide.java

---
+++ src/test/java/github/FindSoftAssertionsWikiPageTest.java
@@ -0,0 +1,35 @@
+package github;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class FindSoftAssertionsWikiPageTest {
+    @Test
+    void junitFiveShouldBePresentFindAssertionViaSearchBar() {
+        // Откройте страницу Selenide в Github
+        open("https://github.com/selenide/selenide");
+        // Перейдите в раздел Wiki проекта
+        $("[data-tab-item=i5wiki-tab").click();
+        // Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $$(".Box-row").findBy(visible).shouldHave(text("SoftAssertions"));
+        $$(".Box-row").findBy(visible).click();
+        // Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
+        $(".gollum-markdown-content").shouldHave(text("Using JUnit5 extend test class"));
+    }
+
+    @Test
+    void junitFiveShouldBePresentFindAssertionByWebElement() {
+        // Откройте страницу Selenide в Github
+        open("https://github.com/selenide/selenide");
+        // Перейдите в раздел Wiki проекта
+        $("[data-tab-item=i5wiki-tab").click();
+        $(".wiki-rightbar li [type=button]").click();
+        // Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
+        $$(".Box-row a").findBy(text("SoftAssertions")).click();
+        // Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
+        $(".gollum-markdown-content").shouldHave(text("Using JUnit5 extend test class"));
+    }
+}
